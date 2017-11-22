a313227335ee1e8a0da628bda426fac43e6f2502
Jacob Marks <jacobjmarks@outlook.com>
2017-11-22 14:38:34 +1000

    Setup some intial front and backend scaffolds

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-    res.end();
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.render('index.pug');
 })
 
 app.listen(PORT, () => {
