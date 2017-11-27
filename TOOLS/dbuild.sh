2b0cb1c69ec74ef38e21bc349bec30027c3dd02d
Jacob Marks <jacobjmarks@outlook.com>
2017-11-27 12:09:13 +1000

    Implemented redirect within tool build script to utilise relative filepaths

--- TOOLS/dbuild.sh
+++ TOOLS/dbuild.sh
@@ -1,4 +1,10 @@
+# Move to script directory to use relative filepaths
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd $SCRIPTPATH
+
+# Clean previous builds/runs
 sh ./dclean.sh
 
 # Tool A
-docker build -t qbio-t_toola ./TOOLS/test/
+docker build -t qbio-t_toola ./test/
