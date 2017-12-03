b00abc84534ba0e64cfffcce7ff6f22526e4c528
Jacob Marks <jacobjmarks@outlook.com>
2017-12-03 17:53:31 +1000

    Tool controller refinements
    More work to be done on standardizing docker exec commands

--- libs/tool-controller.js
+++ libs/tool-controller.js
@@ -1,27 +1,22 @@
 const { exec } = require('child_process');
 const conf = require('../conf.json');
-
-const tools = [
-    {
-        name: "Tool A",
-        func: toolA
-    }
-]
+const fs = require('fs');
 
 module.exports.process = (file, tool, cb) => {
     if (!file) return cb(new Error("Invalid datafile."));
     if (!tool) return cb(new Error("No tool specified."));
 
-    let t = tools.find((t) => t.name == tool);
-    if (!t) return cb(new Error("Invalid tool."));
-
-    t.func(conf.dataDir + file, (err, result) => {
-        if (err) return cb(new Error("Internal tool error."));
-        cb(null, result);
-    });
+    try {
+        this[tool](conf.dataDir + file, (err, result) => {
+            if (err) return cb(new Error("Internal tool error."));
+            cb(null, result);
+        });
+    } catch(err) {
+        cb(new Error("Invalid tool."));
+    }
 }
 
-function toolA(file, cb) {
+module.exports.test = (file, cb) => {
     exec(`docker exec qbio_test bash -c "python test.py '${file}'"`, (error, stdout, stderr) => {
         if (error) {
             console.log(error);
