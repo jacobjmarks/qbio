5d1d6494741a1e0a925efe7d2a24d439ae938174
Jacob Marks <jacobjmarks@outlook.com>
2017-12-05 14:48:32 +1000

    Defined job page layout

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post('/jobStatus', (req, res) => {
 app.get('/job/:id', (req, res) => {
     jobs.get(req.params.id, (err, job) => {
         if (err) return res.status(500).send(err.message);
-        res.send(job);
+        res.render('job.pug', {job: job});
     })
 })
 
