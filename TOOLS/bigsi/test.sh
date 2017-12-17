584f8972526c976827484598c9ca62c0051cb1cd
Jacob Marks <jacobjmarks@outlook.com>
2017-12-17 13:55:57 +1000

    Added BIGSI Dockerfile and test script

---
+++ TOOLS/bigsi/test.sh
@@ -0,0 +1,13 @@
+docker exec qbio_bigsi bash -c '\
+    rm -Rf /data/* && \
+    echo "PREPARING DATA" && \
+    mccortex/bin/mccortex31 build -k 3 -s test1 -1 /src/qbio_data/sequences.fna /data/test1.ctx && \
+    echo "CONSTRUCTING BLOOM FILTERS" && \
+    bigsi init /data/test.bigsi --k 3 --m 1000 --h 1 && \
+    bigsi bloom --db /data/test.bigsi -c /data/test1.ctx /data/test1.bloom && \
+    echo "BUILDING COMBINED GRAPH" && \
+    bigsi build /data/test.bigsi /data/test1.bloom && \
+    echo "QUERYING" && \
+    bigsi search --db /data/test.bigsi -s ACTG && \
+    rm -Rf /data/* \
+'
