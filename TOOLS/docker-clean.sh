ccd3625ab6dc9188ac03623dab2fed14a85b045c
Jacob Marks <jacobjmarks@outlook.com>
2017-12-18 19:52:52 +1000

    Improved automated tool Docker image cleanup

--- TOOLS/docker-clean.sh
+++ TOOLS/docker-clean.sh
@@ -1,3 +1,3 @@
 docker container stop $(docker ps -q -f NAME=qbio_)
 docker container prune -f
-docker image remove $(docker images -q -f REFERENCE=qbio_*)
+docker image remove $(docker images | grep -o 'qbio_[^ ]*')
