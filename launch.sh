57b0f2ba371c8b2eac96571ade3b65a2ca32adfc
Jacob Marks <jacobjmarks@outlook.com>
2017-12-20 22:02:03 +1000

    Added server port variable to launch script

--- launch.sh
+++ launch.sh
@@ -6,6 +6,7 @@ cd $SCRIPTPATH
 
 # CONFIGURATION ------------------------------------
 # Volume binding. Full paths required.
+SERVERPORT=80
 DATADIR='qbio_datavol'
 JOBDIR='qbio_jobvol'
 # --------------------------------------------------
@@ -15,7 +16,7 @@ docker container prune -f
 docker build -t qbio .
 
 docker run -it --rm --name qbio \
-    -p 80:3000 \
+    -p $SERVERPORT:3000 \
     -v /var/run/:/var/run/ \
     -v "$DATADIR":/qbio_data/ \
     -v "$JOBDIR":/qbio_jobs/ \
