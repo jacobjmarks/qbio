275d730347cee4bdfc800ca7c5447d33a3f9406b
Jacob Marks <jacobjmarks@outlook.com>
2018-01-10 18:39:37 +1000

    Setup basic Plotly charting

--- public/js/visualize.js
+++ public/js/visualize.js
@@ -1,3 +1,73 @@
 $(document).ready(() => {
-    
+    populateJobs();
 })
+
+function populateJobs() {
+    $.ajax({
+        method: "GET",
+        url: "jobs/status",
+        success: (data, status, req) => {
+            data.forEach((job) => {
+                $("#jobs").append(
+                    $("<div class='card mb-3 w-100 text-center'>")
+                        .append(
+                            $("<div class='card-body'>")
+                                .html(job.created_at + '<br>' + job.tool)
+                        )
+                        .click(() => {
+                            visualize(job);
+                        })
+                )
+            })
+        }
+    })
+}
+
+function visualize(job) {
+    $.ajax({
+        method: "GET",
+        url: `job/${job.created_at}/result/download`,
+        success: (data, status, req) => {
+            let result = data;
+
+            let div = $("#vis div")[0];
+
+            Plotly.purge(div);
+            
+            switch (job.tool) {
+                case "bloom_filter": (() => {
+                    let tsv = Plotly.d3.tsv.parse(result);
+
+                    let plotvals = {
+                        x: [],
+                        y: []
+                    }
+
+                    tsv.forEach((datum) => {
+                        plotvals.y.push(datum['BFBits']);
+                    })
+
+                    Plotly.plot(div, [{
+                        y: plotvals.y
+                    }], { margin: { t: 0 } })
+                })(); break;
+                case "mmseqs2": (() => {
+                    let tsv = Plotly.d3.tsv.parse(result);
+
+                    let plotvals = {
+                        x: [],
+                        y: []
+                    }
+
+                    tsv.forEach((datum) => {
+                        plotvals.y.push(datum['bitScore']);
+                    })
+
+                    Plotly.plot(div, [{
+                        y: plotvals.y
+                    }], { margin: { t: 0 } })
+                })(); break;
+            }
+        }
+    })
+}
